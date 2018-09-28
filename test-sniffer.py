a3b42072da5874e9a542c07e5bae9abd2adf7dc5
Krieglied <kriegliedthosshys@gmail.com>
2018-09-28 14:54:10 -0500

    Added file output

--- test-sniffer.py
+++ test-sniffer.py
@@ -7,6 +7,7 @@ import socket
 import struct
 import binascii
 import os
+import datetime
 
 def ip_header(data):
     storeobj = struct.unpack("!BBHHHBBH4s4s", data)
@@ -37,11 +38,16 @@ s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
  
 # receive a packet
 while True:
-   # print output on terminal
-   pkt=s.recvfrom(65565)
-
-   print "\n\n[+] ------------ IP Header ------------[+]"
-
-   for i in ip_header(pkt[0][14:34]).iteritems():
-       a, b = i
-       print "{} : {} | ".format(a, b)
+    # print output on terminal
+    pkt=s.recvfrom(65565)
+    file_log = open("packets.log", "a")
+    current_date = str(datetime.datetime.now())
+    file_log.write(current_date) 
+    file_log.write(" ----------")
+    
+    print "\n\n[+] ------------ IP Header ------------[+]"
+    for i in ip_header(pkt[0][14:34]).iteritems():
+        a, b = i
+        file_log.write("{} : {} |".format(a, b))
+        print "{} : {} | ".format(a, b)
+    file_log.close()
