d4793b8d11dd09dcee05c6b9b6a3ec5c3b8c0cc4
Krieglied <kriegliedthosshys@gmail.com>
2018-10-01 13:49:45 -0500

    Build File Name Function

--- test-sniffer.py
+++ test-sniffer.py
@@ -16,6 +16,10 @@ def eth_header(data):
     destination_mac = binascii.hexlify(storeobj[0])
     source_mac = binascii.hexlify(storeobj[1])
     eth_protocol = storeobj[2]
+    destination_mac = ":".join([destination_mac[i:i + 2] 
+        for i in range(0, len(destination_mac), 2)])
+    source_mac = ":".join([source_mac[i:i + 2] 
+        for i in range(0, len(source_mac), 2)])
     data = {"Destination Mac": destination_mac,
             "Source Mac": source_mac,
             "Protocol": eth_protocol}
@@ -57,9 +61,6 @@ def ip_header(data):
             'Destination Address': ip_destination_address}
     return data
 
-
-
-
 # TCP Header
 def tcp_header(data):
     storeobj = struct.unpack('!HHLLBBHHH',data)
@@ -84,7 +85,7 @@ def tcp_header(data):
     return data
 
 # UDP Header
-def udp_header(self, data):
+def udp_header(data):
     storeobj = struct.unpack('!HHHH', data)
     udp_source_port = storeobj[0]
     udp_dest_port = storeobj[1]
@@ -96,23 +97,78 @@ def udp_header(self, data):
             "CheckSum": udp_checksum}
     return data
 
-#create an INET, raw socket
-s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
- 
-# receive a packet
-while True:
-    # print output on terminal
-    pkt=s.recvfrom(65565)
-    file_log = open("packets.log", "a")
+def buildFileName():
+    file_choice = ''
+    while True:
+        print("Would you like to write to a new log file(N)"),
+        user_input = raw_input("or the last one(L)?:::")
+        if user_input.upper() != 'N' and user_input.upper() != 'L':
+            print("User input is not valid.  Please choose again.")
+        else:
+            file_choice = user_input
+            break
+    full_file_name = ""
+    packet_version = 1
+    if file_choice == 'N':
+        file_name = str(datetime.datetime.today().year) + "_"
+        file_name += str(datetime.datetime.today().month) + "_"
+        file_name += str(datetime.datetime.today().day)
+        file_ext =  "_packet.log"
+        list_of_files = os.listdir(os.getcwd())
+        full_file_name = file_name + file_ext
+        while full_file_name in list_of_files:
+            full_file_name = file_name + "_(" + str(packet_version) + ")"
+            full_file_name += file_ext
+            packet_version += 1
+        file_write = open(full_file_name, "w+")
+        file_write.close()
+    if file_choice == 'L':
+        year = 0
+        month = 0
+        day = 0
+        version = 0
+        list_of_files = os.listdir(os.getcwd())
+        for item in list_of_files:
+            parts = item.split(".")
+            if parts[len(parts) - 1] == 'log':
+                if int(item.split("_")[0]) > year:
+                    year = int(item.split("_")[0])
+                if int(item.split("_")[0]) == year:
+                    if int(item.split("_")[1]) > month:
+                        month = int(item.split("_")[1])
+                    if int(item.split("_")[1]) == month:
+                        if int(item.split("_")[2]) > day:
+                            day = int(item.split("_")[2])
+                        if int(item.split("_")[2]) == day:
+                            if "packet" not in item.split("_")[3]:
+                                version = item.split("_")[3]
+        full_file_name = str(year) + "_" + str(month) + "_" + str(day) + "_" + version
+        full_file_name += "_packet.log"
+    return full_file_name
+
+
+def writeToLog(data):
+    # file_log = open("packets.log", "a")
     current_date = str(datetime.datetime.now())
-    file_log.write(current_date) 
-    file_log.write(" ----------")
+    # file_log.write(current_date) 
+    # file_log.write(" ----------")
 
-
-    
+    print "\n\n[+] ------------ Ethernet Header----- [+]"
+    for header in eth_header(data[0][0:14]).iteritems():
+        a, b = header
+        print("{} :{} |").format(a, b)
     print "\n\n[+] ------------ IP Header ------------[+]"
     for i in ip_header(pkt[0][14:34]).iteritems():
         a, b = i
-        file_log.write("{} : {} |".format(a, b))
         print "{} : {} | ".format(a, b)
-    file_log.close()
+
+
+#create an INET, raw socket
+s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
+
+logfile = buildFileName()
+# receive a packet
+while True:
+    # print output on terminal
+    pkt = s.recvfrom(65565)
+    writeToLog(pkt)
