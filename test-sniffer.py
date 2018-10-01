8ff747e9e77a86bc7da69871c6760283bc311317
Krieglied <kriegliedthosshys@gmail.com>
2018-10-01 09:10:42 -0500

    Other Header methods added

--- test-sniffer.py
+++ test-sniffer.py
@@ -9,6 +9,30 @@ import binascii
 import os
 import datetime
 
+# Ethernet Header
+def eth_header(data):
+    storeobj = data
+    storeobj = struct.unpack("!6s6sH", storeobj)
+    destination_mac = binascii.hexlify(storeobj[0])
+    source_mac = binascii.hexlify(storeobj[1])
+    eth_protocol = storeobj[2]
+    data = {"Destination Mac": destination_mac,
+            "Source Mac": source_mac,
+            "Protocol": eth_protocol}
+    return data
+
+# ICMP Header
+def icmp_header(data):
+    icmph = struct.unpack('!BBH', data)
+    icmp_type = icmph[0]
+    code = icmph[1]
+    checksum = icmph[2]
+    data = {'ICMP Type': icmp_type,
+            "Code": code,
+            "CheckSum": checksum}
+    return data
+
+# IP Header
 def ip_header(data):
     storeobj = struct.unpack("!BBHHHBBH4s4s", data)
     ip_version = storeobj[0]
@@ -32,7 +56,46 @@ def ip_header(data):
             'Source Address': ip_source_address,
             'Destination Address': ip_destination_address}
     return data
- 
+
+
+
+
+# TCP Header
+def tcp_header(data):
+    storeobj = struct.unpack('!HHLLBBHHH',data)
+    tcp_source_port = storeobj[0]
+    tcp_destination_port = storeobj[1]
+    tcp_sequence_number = storeobj[2]
+    tcp_acknowledge_number = storeobj[3]
+    tcp_offset_reserved = storeobj[4]
+    tcp_flag = storeobj[5]
+    tcp_window = storeobj[6]
+    tcp_checksum = storeobj[7]
+    tcp_urgent_pointer = storeobj[8]
+    data = {"Source Port": tcp_source_port,
+            "Destination Port": tcp_destination_port,
+            "Sequence Number": tcp_sequence_number,
+            "Acknowledge Number": tcp_acknowledge_number,
+            "Offset &amp; Reserved": tcp_offset_reserved,
+            "Tcp Flag": tcp_flag,
+            "Window": tcp_window,
+            "CheckSum": tcp_checksum,
+            "Urgent Pointer": tcp_urgent_pointer}
+    return data
+
+# UDP Header
+def udp_header(self, data):
+    storeobj = struct.unpack('!HHHH', data)
+    udp_source_port = storeobj[0]
+    udp_dest_port = storeobj[1]
+    udp_length = storeobj[2]
+    udp_checksum = storeobj[3]
+    data = {"Source Port": udp_source_port,
+            "Destination Port": udp_dest_port,
+            "Length": udp_length,
+            "CheckSum": udp_checksum}
+    return data
+
 #create an INET, raw socket
 s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
  
@@ -44,6 +107,8 @@ while True:
     current_date = str(datetime.datetime.now())
     file_log.write(current_date) 
     file_log.write(" ----------")
+
+
     
     print "\n\n[+] ------------ IP Header ------------[+]"
     for i in ip_header(pkt[0][14:34]).iteritems():
