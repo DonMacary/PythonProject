717750a3a55ff815848904ad32665fea41a0465f
wkreiser <williamkreiser@outlook.com>
2018-10-02 11:00:28 -0500

    Report() including proto translation

--- report.py
+++ report.py
@@ -1,38 +1,38 @@
 #This function will display the table while listening
 
-#This import is necessary to call report()
-import report as rp
-import datetime
-
-#This line needs to be inserted into main loop to print the table
-rp.report(pkt)
+from shared_headers import ip_header
+from ipprotoconvert import *
+import time
 
 #Global variables
 line_num = 0
 packet_db = []
 
-def report(data):
+def display_table(data):
     global packet_db
-
-    while True:
-        try:
-            if line_num % 50 == 0:
-                print "\n\n[+[ ----------------------- Macary Madness --------------------------[+]"
-                print "{} | {} | {} | {} | {} | {} | {} |".format(" No.", 
-                "Time", "Source IP", "Destination IP", "Protocol", "Length", "Information")
-            else:
-                dt = datetime.time()
-                rp_data = []
-                for i in ip_header(data[0][14:34]).iteritems():    
-                    a, b = i
-                    rp_data.append(line_num)
-                    rp_data.append(dt)
-                    rp_data.append(b)      
-            print " {} | {} | {} | {} | {} | {} | {}".format(rp_data[0], 
-                rp_data[1], rp_data[2], rp_data[11], 
-                rp_data[5], rp_data[3], 
-                rp_data[10])
+    global line_num
+    
+    try:
+        if line_num % 50 == 0:
+            print "\n\n  [+] ------------------------------- Macary Madness ------------------------------ [+]"
+            print " {:4} | {:8} | {:16} | {:16} | {:8} | {:6} | {:8} ".format(" No.", 
+            "Time", "Source IP", "Destination IP", "Protocol", "Length", "Information")
+        else:
+            dt = time.strftime("%H:%M:%S")
+            rp_data = []
+            for i in ip_header(data[0][14:34]).iteritems():    
+                a, b = i
+                rp_data.append(b)     
+            rp_data.insert(0, line_num)
+            rp_data.insert(1, dt)
+                
+            print " {:4} | {:8} | {:16} | {:16} | {:8} | {:6} | {:8} ".format(rp_data[0], 
+            rp_data[1], rp_data[2], rp_data[11], 
+            protoName(rp_data[5]), rp_data[3], 
+            rp_data[10])
             packet_db.append(rp_data)
-            line_num += 1
-        except KeyboardInterrupt:
-            break
+        line_num += 1
+    except KeyboardInterrupt:
+        #This needs to be replaced with main() call after main is built
+        exit(0)
+        # main()
