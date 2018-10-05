5f2188e69b52c761530a1068473e5bada08faf70
wkreiser <williamkreiser@outlook.com>
2018-10-05 11:06:31 -0500

    commented enumPacket

--- enumPacket.py
+++ enumPacket.py
@@ -8,21 +8,27 @@ def packetCombo():
     print "Separate packet number by comma (1,2) and range by dashes (4-6)"
     user_input = raw_input("Enter packet number and/or packet number range: ").rstrip()
     try:
+        #look for single int first and split by comma
         packRange = user_input.split(",")
         j = []
         for i in range(len(packRange)):
             try:
+                #make sure value is int
                 packRange[i] = int(packRange[i])
+                #put number in temp list j
                 j.append(packRange[i])
-            except:                
+            except:     
+                #look for ranges and split by dash           
                 temp = packRange[i].split("-")
+                #start at first number before split, continues until last number, step by 1
                 for num in range(int(temp[0]), (int(temp[1])+1), 1):
                     j.append(num)                
     except ValueError:
         print ("Please input a valid response.")
         user_input = packetCombo()
+    #return temp list j
     return j
-
+#This function will unpack all of the headers in a packet
 def enumeratePacket(data):
     temp_db = []
     #Ethernet Header
@@ -48,7 +54,7 @@ def enumeratePacket(data):
         print "\n--------------------------------"
         print "----------- TCP HEADER ---------"
         print "--------------------------------"
-        #checks data offset for more than 5 words
+        #Unpacks the TCP Header
         for i in tcp_header(data[0][34:54]).iteritems():                
             a, b = i
             print "{} : {} ".format(a,b)         
@@ -58,7 +64,7 @@ def enumeratePacket(data):
         print "\n--------------------------------"
         print "----------- UDP HEADER ---------"
         print "--------------------------------" 
-
+        #Unpacks UDP Header
         for i in udp_header(data[0][34:42]).iteritems():
             a, b = i
             print "{} : {} ".format(a,b)        
@@ -68,7 +74,7 @@ def enumeratePacket(data):
         print "\n--------------------------------"
         print "---------- ICMP HEADER ---------"
         print "--------------------------------" 
-
+        #Unpacks the ICMP header
         for i in icmp_header(data[0][34:38]).iteritems():
             a, b = i
             print "{} : {} ".format(a,b)
@@ -87,40 +93,47 @@ def check_int():
 def packetChoice(data):
     #data is now a list of packets   
     runMenu = True
-
+    #Prints menu 
     while runMenu == True:
-        print "\nEnumeration Station"
-        print "1. Choose packets"
-        print("2. Previous Menu")
-        print("{:_^20}").format("")
-        userInput = check_int()
-        if (userInput == 1):
-            #Allows for single and packet range selection
-            invalid_int = False
-            pkCombo = packetCombo()
-            #Checking that packet is in range 
-            for item in pkCombo:
-                if item < 0 or item > len(data):
-                    invalid_int = True
+        try:
+            print "\nEnumeration Station"
+            print "1. Choose packets"
+            print("2. Previous Menu")
+            print("{:_^20}").format("")
+            userInput = check_int()
+            if (userInput == 1):
+                #Allows for single and packet range selection
+                invalid_int = False
+                pkCombo = packetCombo()
+                #Checking that packet is in range 
+                for item in pkCombo:
+                    if item < 0 or item > len(data):
+                        invalid_int = True
+                        break
+                #Making sure pkCombo exists
+                if not pkCombo:
+                    print "\nNo packets selected"
+                    packetChoice(data)
                     break
-            #Making sure pkCombo exists
-            if not pkCombo:
-                print "\nNo packets selected"
-                packetChoice(data)
+                #Packet selected < 0 or packet > number of packets in database
+                elif invalid_int:
+                    print "Packet number out of range."
+                    packetChoice(data)
+                    break
+                elif pkCombo:
+                    #This calls enumerate function for each packet selected
+                    for i in pkCombo:                    
+                        print "\nPacket {}".format(i)
+                        enumeratePacket(data[i -1])
+                        time.sleep(1)                            
+            elif (userInput == 2):
                 break
-            #Packet selected < 0 or packet > number of packets in database
-            elif invalid_int:
-                print "Packet number out of range."
-                packetChoice(data)
-                break
-            elif pkCombo:
-                for i in pkCombo:                    
-                    print "\nPacket {}".format(i)
-                    enumeratePacket(data[i -1])
-                    time.sleep(1)                            
-        elif (userInput == 2):
+            else:
+                print("{:_^20}").format("")
+                print("That was not a valid option.")
+                print("{:_^20}").format("")
+        #Catch those pesky Ctrl-C
+        except KeyboardInterrupt:
+            print "\nAH AH AH you didn't say the magic word!!!"
+            packetChoice(data)
             break
-        else:
-            print("{:_^20}").format("")
-            print("That was not a valid option.")
-            print("{:_^20}").format("")
