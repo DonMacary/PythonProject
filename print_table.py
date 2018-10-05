f033e0ebe9e32d8582cee3aab223df2e774cc6ff
dariusrenj <dariusrenj@gmail.com>
2018-10-05 10:41:33 -0500

    Updated try/except to account for keyboard interrupt

--- print_table.py
+++ print_table.py
@@ -8,28 +8,31 @@ def displayTable():
     #open file
     itter = 1
     while itter == 1:
-       try:
+        try:
            #open file
            input_file = open(raw_input("Type the file you would like to open.\n"), 'rU')
            itter = 2
-       except:
+        except KeyboardInterrupt:
+            break
+        except:
            print("Invalid File Name.  Please try again.")
-    #print table header
-    print "\n\n  [+] ------------------------------- Macary Madness ------------------------------ [+]"
-    print " {:4} | {:8} | {:16} | {:16} | {:8} | {:6} | {:8} ".format(" No.", 
-    "Time", "Source IP", "Destination IP", "Protocol", "Length", "Information")
-    #read data from file
-    data = input_file.readlines()
-    #loop through and display data read from file
-    for y in range(len(data)):
-        sourceData = data[y].split(' ')
-        sourceIP = sourceData[13]
-        destIP = sourceData[14]
-        protocol = protoName(int(sourceData[11]))
-        length = sourceData[7]
-        info = sourceData[12]
+    if (itter == 2):
+        #print table header
+        print "\n\n  [+] ------------------------------- Macary Madness ------------------------------ [+]"
+        print " {:4} | {:8} | {:16} | {:16} | {:8} | {:6} | {:8} ".format(" No.", 
+        "Time", "Source IP", "Destination IP", "Protocol", "Length", "Information")
+        #read data from file
+        data = input_file.readlines()
+        #loop through and display data read from file
+        for y in range(len(data)):
+            sourceData = data[y].split(' ')
+            sourceIP = sourceData[13]
+            destIP = sourceData[14]
+            protocol = protoName(int(sourceData[11]))
+            length = sourceData[7]
+            info = sourceData[12]
 
-        print (" {:4} | {:8} | {:16} | {:16} | {:8} | {:6} | {:8} ").format((y+1), (data[y][11:19]),sourceIP,
-        destIP, protocol, length, info)
-    #close file
-    input_file.close()
+            print (" {:4} | {:8} | {:16} | {:16} | {:8} | {:6} | {:8} ").format((y+1), (data[y][11:19]),sourceIP,
+            destIP, protocol, length, info)
+        #close file
+        input_file.close()
